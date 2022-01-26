facaaa3d7fe42b9374ec861832d8ff4372e43949
TIberiu99p <paharnicutiberiu@gmail.com>
2022-01-26 15:40:44 +0000

    Amity card animation

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Text, Pressable} from 'react-native';
 import Card from './source/components/AmityCard';
 import users from './TinderAssets/assets/data/users';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+} from 'react-native-reanimated';
 
 const App = () => {
+  const shareValue = useSharedValue(1);
+
+  const cardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: shareValue.value * 500 - 250,
+      },
+    ],
+  }));
+
   return (
     <View style={styles.cardContainer}>
-      <Card user={users[1]} />
+      <Animated.View style={(styles.cardAnimation, cardStyle)}>
+        <Card user={users[2]} />
+      </Animated.View>
+      <Pressable onPress={() => (shareValue.value = withSpring(Math.random()))}>
+        <Text>Change value</Text>
+      </Pressable>
     </View>
   );
 };
@@ -16,6 +37,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
+  },
+  cardAnimation: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
